79670c47734fd7febee66d78c76bd0a15b8631a0
smihaylov69 <smihaylov208@gmail.com>
2024-02-09 12:03:16 +0200

    Среда за разработка

---
+++ Среда за разработка/zad5.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int removeDuplicates(int* nums, int numsSize);
+
+int main(void) {
+  int nums[10] = {0, 0, 0, 1, 1, 2, 2, 5, 5, 7};
+  int size = removeDuplicates(nums, 10);
+  for (int i = 0; i < size; i++) {
+    printf("%d ", nums[i]);
+  }
+
+  return 0;
+}
+
+int removeDuplicates(int* nums, int numsSize){
+    int k = 1;
+    for (int i = 1; i < numsSize; i++) {
+        if (nums[i] != nums[i-1]) {
+            nums[k] = nums[i];
+            k++;
+        }
+    }
+    return k;
+}
